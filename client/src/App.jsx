ccd7d831847bbe469138938c662310a4e9cf7f9b
coenzeng <63679316+coenzeng@users.noreply.github.com>
2020-08-17 16:34:02 -0400

    create react router, import bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route} from 'reat-router-dom'
+import Home from "./routes/Home"
+import UpdatePage from "./routes/UpdatePage"
+import RestaurantsDetailPage from "./routes/RestaurantDetailPage"
 
 const App = () => {
     return(
-        <div>App</div>
+        <div>
+            <Router>
+                <Switch>
+                <Route exact path ="/" component={Home}/>
+                <Route exact path ="/restaurants/:id/update" component={UpdatePage}/>
+                <Route exact path ="/restaurants/:id" component={RestaurantsDetailPage}/>
+                </Switch>
+            </Router>
+        </div>
     )
 }
 
---
+++ client/src/routes/UpdatePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UpdatePage = () => {
+    return(
+        <div>Update Page</div>
+    )
+}
+
+export default UpdatePage;
