4f63c2510b2584032d7300502c7a2824f70a5323
coenzeng <63679316+coenzeng@users.noreply.github.com>
2020-08-23 21:06:32 -0400

    save current changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from "./routes/Home"
@@ -25,36 +24,4 @@ const App = () => {
 }
 
 export default App;
-=======
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./routes/Home";
-import UpdatePage from "./routes/UpdatePage";
-import RestaurantDetailPage from "./routes/RestaurantDetailPage";
-import { RestaurantsContextProvider } from "./context/RestaurantsContext";
-const App = () => {
-  return (
-    <RestaurantsContextProvider>
-      <div className="container">
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route
-              exact
-              path="/restaurants/:id/update"
-              component={UpdatePage}
-            />
-            <Route
-              exact
-              path="/restaurants/:id"
-              component={RestaurantDetailPage}
-            />
-          </Switch>
-        </Router>
-      </div>
-    </RestaurantsContextProvider>
-  );
-};
 
-export default App;
->>>>>>> d263a669e8d4e1339f849a111e906a1b003d64e3
--- client/src/context/RestaurantsContext.js
+++ client/src/context/RestaurantsContext.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, createContext } from "react";
 
 export const RestaurantsContext = createContext();
@@ -24,30 +23,4 @@ export const RestaurantsContextProvider = (props) => {
     </RestaurantsContext.Provider>
   );
 };
-=======
-import React, { useState, createContext } from "react";
 
-export const RestaurantsContext = createContext();
-
-export const RestaurantsContextProvider = (props) => {
-  const [restaurants, setRestaurants] = useState([]);
-  const [selectedRestaurant, setSelectedRestaurant] = useState(null);
-
-  const addRestaurants = (restaurant) => {
-    setRestaurants([...restaurants, restaurant]);
-  };
-  return (
-    <RestaurantsContext.Provider
-      value={{
-        restaurants,
-        setRestaurants,
-        addRestaurants,
-        selectedRestaurant,
-        setSelectedRestaurant,
-      }}
-    >
-      {props.children}
-    </RestaurantsContext.Provider>
-  );
-};
->>>>>>> d263a669e8d4e1339f849a111e906a1b003d64e3
